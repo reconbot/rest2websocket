f5b380e750e477a75db3768fbe4e5aeb9cdcf6cf
Francis Gulotta <wizard@roborooter.com>
2012-08-18 19:56:01 -0400

    resource server and tests

---
+++ resource-server.js
@@ -0,0 +1,94 @@
+var EventEmitter2 = require('EventEmitter2').EventEmitter2;
+var util = require("util");
+
+/* Catalog of events
+  connect -> client
+  disconnect -> client
+  subscribe -> resource
+  unsubscribe -> resource
+*/
+
+var ResourceServer = function(){
+  this.resources = {};
+  EventEmitter2.apply(this, arguments);
+  this.setMaxListeners(0); //unlimited
+  this.clients = {};
+  this.resources = {};
+};
+util.inherits(ResourceServer, EventEmitter2);
+
+var clientID = function(client){
+  return client.id;
+};
+
+ResourceServer.prototype.connect = function(client){
+  var id = clientID(client);
+  if(this.clients[id]){ return; }
+  
+  var sendData = function(data){
+    client.emit('data', data);
+  };
+
+  var sub = (function(resource){
+    if(this.clients[id].subs[resource]){
+      return;
+    }
+    this.clients[id].subs[resource] = true;
+    this.on(resource, sendData);
+
+    if(!this.resources[resource]){
+      this.resources[resource] = 1;
+      this.emit('subscribe', resource);
+    }else{
+      this.resources[resource] ++;
+    }
+  }).bind(this);
+
+  var unsub = (function(resource){
+    if(!this.clients[id].subs[resource]){
+      return;
+    }
+
+    this.off(resource, sendData);
+    this.resources[resource] --;
+    delete this.clients[id].subs[resource];
+
+    if(this.resources[resource] <= 0){
+      delete this.resources[resource];
+      this.emit('unsubscribe', resource);
+    }
+
+  }).bind(this);
+
+  this.clients[id] = {
+    subs: {},
+    id: id,
+    sendData: sendData,
+    sub: sub,
+    unsub: unsub
+  };
+
+  client.on('subscribe',sub);
+  client.on('unsubscribe', unsub);
+
+  this.emit('connect', client);
+};
+
+ResourceServer.prototype.disconnect = function(client){
+  var id = clientID(client);
+  var data = this.clients[id];
+  for(var resource in data.subs){
+    data.unsub(resource);
+  }
+  client.off('subscribe', data.sub);
+  client.off('unsubscribe', data.unsub);
+  delete this.clients[id];
+
+  this.emit('disconnect', client);
+};
+
+ResourceServer.prototype.publish = function(data){
+  this.emit(data.resource, data);
+};
+
+module.exports = ResourceServer;
---
+++ test/resource-server-general.js
@@ -0,0 +1,95 @@
+var test = require('tap').test;
+var ResourceServer = require('../resource-server');
+var EventEmitter2 = require('EventEmitter2').EventEmitter2;
+
+test('startup', function(t){
+  t.ok(ResourceServer,'event server was loaded');
+  t.ok(new ResourceServer(), 'event server was created');
+  t.end();
+});
+
+
+test('clients come and go',function(t){
+  t.plan(4);
+  var client = new EventEmitter2();
+  client.id = 'bob';
+  var rs = new ResourceServer();
+
+  rs.on('connect',function(c){
+    t.equal(c, client, 'client connection event');
+  });
+
+  rs.on('disconnect',function(c){
+    t.equal(c, client, 'client disconnect event');
+  });
+
+  rs.connect(client);
+  t.ok(rs.clients.bob, 'client was added');
+
+  rs.disconnect(client);
+  t.ok(!rs.clients.bob, 'client was removed');
+  t.end();
+});
+
+test('clients subscribe so many times', function(t){
+  t.plan(2);
+  var client = new EventEmitter2();
+  client.id = 'bob';
+  var rs = new ResourceServer();
+  rs.connect(client);
+  var resource = 'http://fake.email.com/emails/44';
+
+  rs.on('subscribe', function(res){
+    t.equal(res, resource, 'subscribe event fired');
+  });
+  rs.on('unsubscribe', function(res){
+    t.equal(res, resource, 'unsubscribe event fired');
+  });
+
+  client.emit('subscribe', resource);
+  client.emit('subscribe', resource);
+  client.emit('unsubscribe', resource);
+  client.emit('unsubscribe', resource);
+  t.end();
+});
+
+test('clients disconnect before unsubscribing', function(t){
+  t.plan(2);
+  var client = new EventEmitter2();
+  client.id = 'bob';
+  var rs = new ResourceServer();
+  rs.connect(client);
+  var resource = 'http://fake.email.com/emails/44';
+
+  rs.on('subscribe', function(res){
+    t.equal(res, resource, 'subscribe event fired');
+  });
+  rs.on('unsubscribe', function(res){
+    t.equal(res, resource, 'unsubscribe event fired');
+  });
+
+  client.emit('subscribe', resource);
+  rs.disconnect(client);
+  t.end();
+});
+
+test('publish some data to a client',function(t){
+  t.plan(1);
+  var client = new EventEmitter2();
+  client.id = 'bob';
+  var rs = new ResourceServer();
+  rs.connect(client);
+  var resource = 'http://fake.email.com/emails/44';
+  var data = {
+    resource: resource,
+    data: 'foobar!'
+  };
+  client.emit('subscribe', resource);
+
+  client.on('data', function(d){
+    t.equal(d, data, 'Data is emitted');
+  });
+  rs.publish(data);
+  client.emit('unsubscribe', resource);
+  rs.publish(data);
+});
